88b9ee450edc20390bf27fd89cdfbb9c4609116e
Stephen Pearson <github@croughsnest.com>
2025-08-01 19:10:09 -0700

    Update second.py

--- second.py
+++ second.py
@@ -6,5 +6,6 @@ print( "Currently in Second.py")
 
 from sys import platform    # Load ONLY the platform from the sys library module
 print(platform)
+print("test 2")
 
 # raw_input('Press Enter to exit')
